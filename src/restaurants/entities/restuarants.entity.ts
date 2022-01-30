4fc26a8438e0affd524cd3214e0c94999fdad69c
DevelopJKong <ybaram40@gmail.com>
2022-01-30 11:48:30 +0900

    #1.3 Arguments 완료

--- src/restaurants/entities/restuarants.entity.ts
+++ src/restaurants/entities/restuarants.entity.ts
@@ -2,9 +2,9 @@ import { Field, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
 export class Restaurant {
-  @Field(type => String)
+  @Field((type) => String)
   name: string;
 
-  @Field(type => Boolean, { nullable: true })
+  @Field((type) => Boolean, { nullable: true })
   isGood?: boolean;
 }
