64db62e6b5b63c80b2d82f59bc57551dc4e7f809
DevelopJKong <ybaram40@gmail.com>
2022-01-30 18:53:31 +0900

    #1.4 InputTypes and ArgumentTypes 완료

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,11 +1,17 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver, Mutation } from '@nestjs/graphql';
+import { createRestaurantDto } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entities/restuarants.entity';
 
-@Resolver(of => Restaurant)
+@Resolver((of) => Restaurant)
 export class RestaurantResolver {
-  @Query(returns => [Restaurant])
+  @Query((returns) => [Restaurant])
   restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
     console.log(veganOnly);
     return [];
+  }
+  @Mutation((returns) => Boolean) //왜 여기서 import가 안되었을까?
+  createRestaurant(@Args() createRestaurantDto: createRestaurantDto): boolean {
+    console.log(createRestaurantDto);
+    return true;
   }
 }
