c89345086f52ad6f556cae22ef5671a850b54292
DevelopJKong <ybaram40@gmail.com>
2022-01-31 16:21:43 +0900

    #1.5 Validating ArgsTypes 완료

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   const PORT = 3000;
   const start = () => console.log(`😎 http://localhost:${PORT}`);
-  await app.listen(PORT,start);
+  await app.listen(PORT, start);
 }
 bootstrap();
--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,13 +1,23 @@
-import { ArgsType, Field, InputType } from "@nestjs/graphql";
+import { ArgsType, Field, InputType } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 @ArgsType()
-export class createRestaurantDto {
-    @Field(type => String)
-    name:string
-    @Field(type => Boolean)
-    isVegan:string
-    @Field(type => String)
-    address:string
-    @Field(type => String)
-    ownerName:string
+export class createRestaurantDto { // 그러게? 왜 name 옆 속성은 소문자고 리턴 타입은 대문자지?
+  @Field((type) => String)
+  @IsString()
+  @Length(5,10)
+  name: string;
+
+  @Field((type) => Boolean) 
+  @IsBoolean()
+  isVegan: boolean;
+
+  @Field((type) => String)
+  @IsString()
+  address: string;
+
+  @Field((type) => String)
+  @IsString()
+  @Length(5,10)
+  ownerName: string;
 }
