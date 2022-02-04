9a19ed6026b1d3cbb148c710a9fd59a654510c57
DevelopJKong <ybaram40@gmail.com>
2022-02-04 21:09:32 +0900

    #2.5 Configuring ConfigService 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
@@ -6,18 +7,23 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host:'localhost',
-      port: 5432,
-      username : 'postgres',
-      password : '1234',
-      database: 'number-eats',
-      synchronize : true,
-      logging: true
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT, 
+      username: process.env.DB_USERNAME, 
+      password: process.env.DB_PASSWORD, 
+      database: process.env.DB_NAME, 
+      synchronize: true,
+      logging: true,
     }),
     RestaurantsModule,
   ],
