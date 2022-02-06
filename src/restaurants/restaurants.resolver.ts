678495cdd2a33d4db948aa4b6d361449b05ba055
DevelopJKong <ybaram40@gmail.com>
2022-02-06 17:48:18 +0900

    #3.2 Injecting The Repository 완료

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,13 +1,16 @@
 import { Args, Query, Resolver, Mutation } from '@nestjs/graphql';
 import { createRestaurantDto } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entities/restuarants.entity';
+import { RestaurantService } from './restaurants.service';
 
 @Resolver((of) => Restaurant)
 export class RestaurantResolver {
+  constructor(private readonly restaurantService: RestaurantService) {
+    
+  }
   @Query((returns) => [Restaurant])
-  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    console.log(veganOnly);
-    return [];
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantService.getAll();
   }
   @Mutation((returns) => Boolean) //왜 여기서 import가 안되었을까?
   createRestaurant(@Args() createRestaurantDto: createRestaurantDto): boolean {
